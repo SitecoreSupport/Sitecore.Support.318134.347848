d0890612162179780c1999bace1d68b6be2fa702
Paul Stupka <paul.stupka@sitecore.com>
2019-08-04 13:05:30 +0300

    empty project

---
+++ src/Sitecore.Support.318134.347848/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.318134.347848")]
+[assembly: AssemblyProduct("Sitecore.Support.318134.347848")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Profiling should be disabled by default")]
+
